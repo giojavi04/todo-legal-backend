23bf281b3c316bb585f7cb6c3fc908547b73666f
Javier Montalvo <giojavi04@gmail.com>
2021-06-04 21:37:18 -0500

    save user new

--- app/main/model/user.py
+++ app/main/model/user.py
@@ -20,7 +20,7 @@ class User(db.Model):
     phone_number = db.Column(db.String(80), nullable=True)
     password = db.Column(db.Text, nullable=False)
     password_hash = db.Column(db.String(100))
-    created_at = db.Column(db.DateTime, default=datetime)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.now)
 
     @property
     def password(self):
--- app/main/service/user_service.py
+++ app/main/service/user_service.py
@@ -1,5 +1,4 @@
 import uuid
-import datetime
 
 from app.main import db
 from app.main.model.user import User
@@ -10,11 +9,13 @@ def save_new_user(data: Dict[str, str]) -> Tuple[Dict[str, str], int]:
     user = User.query.filter_by(email=data['email']).first()
     if not user:
         new_user = User(
-            public_id=str(uuid.uuid4()),
+            uuid=str(uuid.uuid4()),
             email=data['email'],
             username=data['username'],
             password=data['password'],
-            registered_on=datetime.datetime.utcnow()
+            name=data['name'],
+            last_name=data['last_name'],
+            phone_number=data['phone_number'],
         )
         save_changes(new_user)
         return generate_token(new_user)
@@ -30,14 +31,11 @@ def get_all_users():
     return User.query.all()
 
 
-def get_a_user(public_id):
-    return User.query.filter_by(public_id=public_id).first()
-
-
 def generate_token(user: User) -> Tuple[Dict[str, str], int]:
     try:
         # generate the auth token
         auth_token = User.encode_auth_token(user.id)
+        print(auth_token)
         response_object = {
             'status': 'success',
             'message': 'Successfully registered.',
@@ -45,6 +43,7 @@ def generate_token(user: User) -> Tuple[Dict[str, str], int]:
         }
         return response_object, 201
     except Exception as e:
+        print(e)
         response_object = {
             'status': 'fail',
             'message': 'Some error occurred. Please try again.'
